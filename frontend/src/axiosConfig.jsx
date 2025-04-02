df962d4dbdb7c4b4824ba0937986eabcfb635216
Lovope <zhang314hh@163.com>
2025-04-02 12:56:15 +1000

    axios debugging

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,4 +1,4 @@
-const axios = require('axios');
+import axios from 'axios';
 
 const instance = axios.create({
   baseURL: 'http://localhost:5001', // local
@@ -6,20 +6,20 @@ const instance = axios.create({
   headers: { 'Content-Type': 'application/json' },
 });
 
-instance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      console.log('Token being used:', token);
-      config.headers['Authorization'] = `Bearer ${token}`;
-    } else {
-      console.warn('No token found in localStorage');
-    }
-    return config;
-  },
-  (error) => {
-    console.error('Axios request error:', error);
-    return Promise.reject(error);
-  }
-);
+// instance.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem('token');
+//     if (token) {
+//       console.log('Token being used:', token);
+//       config.headers['Authorization'] = `Bearer ${token}`;
+//     } else {
+//       console.warn('No token found in localStorage');
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     console.error('Axios request error:', error);
+//     return Promise.reject(error);
+//   }
+// );
 export default instance;
