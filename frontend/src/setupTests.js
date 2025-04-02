05966076b3d65efa6b7312f2dd5c56d5dbc028db
Lovope <zhang314hh@163.com>
2025-04-02 12:31:44 +1000

    fixed the frontend test

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -2,4 +2,4 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+fetch('/api/jobs').then(res => res.json()).then(data => console.log(data));
